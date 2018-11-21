509a18f268e8122a7a3eed0ff14bac6c7f0b3451
Steve Bachmeier <steve.d.bachmeier@gmail.com>
2018-11-20 16:20:42 -0800

    Finish variable paring

--- 02 - Variable Reduction.py
+++ 02 - Variable Reduction.py
@@ -10,6 +10,7 @@ Created on Fri Nov 16 12:19:42 2018
 # ========================================
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.feature_selection import VarianceThreshold
 
@@ -70,4 +71,30 @@ df_corr_variable_outcome[
 df.drop(columns='spotlight', inplace=True)
 
 # ---- VARIABLE-VARIABLE CORRELATIONS ----
+corMat = abs(df.drop(columns=info_variables).drop(columns='launch_state').corr())
+corMat_upper = corMat.where(np.triu(np.ones(corMat.shape), k=1).astype(np.bool))
+corMat_values = corMat_upper.values[np.triu_indices(corMat_upper.shape[1], k=1)]
 
+plt.scatter(x=range(0,len(corMat_values)),y=corMat_values, s=100)
+plt.axhline(y=0.5, color='r', linestyle='--', linewidth=4)
+
+corMat.max().max() # Includes diagonal of ones
+corMat_upper.max().max() # Maximum non-diagonal correlation (>threshold?)
+
+
+
+# Find all variable pairs that have correlation > threshold
+corMat_upper.unstack().sort_values(
+        kind='quicksort')[corMat_upper.unstack().sort_values(
+                kind='quicksort') > corr_threshold]
+
+# =============================================================================
+# Only US and GB variables are correlated above the threshold. However, it does
+# not make sense to drop one of them since they are both countries - the
+# high correlation is purely coincidental.
+# =============================================================================
+
+# ========================================
+# SAVE CSV
+# ========================================
+df.to_csv('data/df02.csv', sep=",")
